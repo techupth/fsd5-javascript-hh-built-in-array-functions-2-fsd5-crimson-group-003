f2ac84eb7eb627bba18be79b83c434e783872ba4
devsomsak <devrleang@gmail.com>
2023-12-12 18:10:53 +0700

    feat:revise Palindrome ex-7

--- ex-7.js
+++ ex-7.js
@@ -1,27 +1,44 @@
-function isPalindrome(string) {
-  let arrangeStr = [];
-  let newStr = [];
+function checkPalindrome(str) {
+  const lowerCaseStr = str.toLowerCase();
+ 
+  const cleanedStr = lowerCaseStr.replace(/[^a-zA-Zก-ฮ]/g, "");
 
-  for (let i = 0; i < string.length; i++) {
-    arrangeStr = string[i];
-    console.log(arrangeStr);
-    if (/[a-zA-Zก-ฮ]/.test(arrangeStr)) {
-      console.log(arrangeStr);
-      newStr += arrangeStr.toLowerCase();
-      console.log(newStr);
-    }
-  }
-
-  for (let i = 0; i < newStr.length / 2; i++) {
-    console.log(newStr.length / 2);
-    if (newStr[i] !== newStr[newStr.length - 1 - i]) {
-      return false;
-    }
-  }
-  return true;
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+  
 }
-
 //Example case
-console.log(isPalindrome("reviver"));// true
-console.log(isPalindrome("บวบ"));// true
-console.log(isPalindrome("deliver"));// false
+console.log(checkPalindrome("reviver"));// true
+console.log(checkPalindrome("บวบ"));// true
+console.log(checkPalindrome("deliver"));// false
+
+
+
+//-Use For Loop-------Palindrome--------------------------------
+
+// function isPalindrome(string) {
+//   let arrangeStr = [];
+//   let newStr = [];
+
+//   for (let i = 0; i < string.length; i++) {
+//     arrangeStr = string[i];
+//     console.log(arrangeStr);
+//     if (/[a-zA-Zก-ฮ]/.test(arrangeStr)) {
+//       console.log(arrangeStr);
+//       newStr += arrangeStr.toLowerCase();
+//       console.log(newStr);
+//     }
+//   }
+
+//   for (let i = 0; i < newStr.length / 2; i++) {
+//     console.log(newStr.length / 2);
+//     if (newStr[i] !== newStr[newStr.length - 1 - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// //Example case
+// console.log(isPalindrome("reviver"));// true
+// console.log(isPalindrome("บวบ"));// true
+// console.log(isPalindrome("deliver"));// false
